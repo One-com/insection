c8ed4cc7c63288375e74291723801be37fc9db9c
Sune Simonsen <sune@we-knowhow.dk>
2014-12-11 10:02:59 +0100

    Fixed quoting

--- test/Insection.spec.js
+++ test/Insection.spec.js
@@ -19,24 +19,24 @@ var expect = require('unexpected').clone()
     });
 
 
-describe('Insection', function () {
+describe("Insection", function () {
     var insection;
     beforeEach(function () {
         insection = new Insection();
     });
 
-    describe('constructor', function () {
-        it('returns an instance of insection', function () {
+    describe("constructor", function () {
+        it("returns an instance of insection", function () {
             expect(insection, 'to be an', 'Insection');
         });
 
-        it('is empty before any intervals has been added', function () {
+        it("is empty before any intervals has been added", function () {
             expect(insection, 'to be empty');
         });
     });
 
-    describe('interval', function () {
-        it('Insection.interval(3,4) is an alias for Insection.interval("[",3,4,"]")', function () {
+    describe("interval", function () {
+        it("Insection.interval(3,4) is an alias for Insection.interval('[',3,4,']')", function () {
             expect(Insection.interval(3, 4), 'to equal', Insection.interval('[', 3, 4, ']'));
         });
 
@@ -48,29 +48,29 @@ describe('Insection', function () {
             { interval: Insection.interval('[', 4, Infinity, ')'), toString: '[4,Infinity)' },
             { interval: Insection.interval('(', -Infinity, Infinity, ')'), toString: '(-Infinity,Infinity)' }
         ].forEach(function (example) {
-            it(example.interval.toString(true) + ' returns the interval ' + example.toString, function () {
+            it(example.interval.toString(true) + " returns the interval " + example.toString, function () {
                 expect(example.interval.toString(), 'to equal', example.toString);
             });
         });
     });
 
-    describe('add', function () {
-        it('add(3,4) is an alias for add(Insection.interval("[",3,4,"]"))', function () {
+    describe("add", function () {
+        it("add(3,4) is an alias for add(Insection.interval('[',3,4,']'))", function () {
             insection.add(3, 4);
             expect(insection, 'to contain', Insection.interval('[', 3, 4, ']'));
         });
 
         [
-            Insection.interval("[", 3, 4, "]"),
-            Insection.interval("[", 3, 4, ")"),
-            Insection.interval("(", 3, 4, "]"),
-            Insection.interval("(", 3, 4, ")")
+            Insection.interval('[', 3, 4, ']'),
+            Insection.interval('[', 3, 4, ')'),
+            Insection.interval('(', 3, 4, ']'),
+            Insection.interval('(', 3, 4, ')')
         ].forEach(function (interval) {
             var startString = interval.startString;
             var start = interval.start;
             var end = interval.end;
             var endString = interval.endString;
-            it('add(' + ['"' + startString + '"', start, end, '"' + endString + '"'].join(',') + ') is an alias for add(' + interval.toString(true) + ')', function () {
+            it("add(" + ["'" + startString + "'", start, end, "'" + endString + "'"].join(",") + ") is an alias for add(" + interval.toString(true) + ")", function () {
                 insection.add(startString, start, end, endString);
                 expect(insection, 'to contain', interval);
             });
@@ -83,7 +83,7 @@ describe('Insection', function () {
             Insection.interval(4, 3345),
             Insection.interval('[', 4, Infinity, ')')
         ].forEach(function (interval) {
-            it('add(' + interval.toString(true) + ') adds the ' + interval + ' interval to the interval collection', function () {
+            it("add(" + interval.toString(true) + ") adds the"  + interval + "interval to the interval collection", function () {
                 expect(insection.add(interval), 'to be an', 'Insection');
                 expect(insection, 'not to be empty');
                 expect(insection, 'to contain', interval);
